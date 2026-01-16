136aa10f1926a158c7ff762b616476081147f7ae
SteveBinary <60712092+SteveBinary@users.noreply.github.com>
2026-01-16 19:39:43 +0100

    Add `nixpkgs` and `nixpkgs-stable` to the flake registry.

--- modules/nixos/nix/default.nix
+++ modules/nixos/nix/default.nix
@@ -21,11 +21,10 @@ in
   config = lib.mkIf cfg.enable {
     nix = {
       package = pkgs.nixVersions.latest;
-      # TODO: this leads to a conflict on orville
-      #registry = {
-      #  nixpkgs.flake = inputs.nixpkgs;
-      #  # nixpkgs-stable.flake = inputs.nixpkgs-stable;
-      #};
+      registry = {
+        nixpkgs.flake = inputs.nixpkgs;
+        nixpkgs-stable.flake = inputs.nixpkgs-stable;
+      };
       channel.enable = false; # remove nix-channel related tools & configs, as flakes are used for everything
       settings = {
         warn-dirty = false;
