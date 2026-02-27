7b6c5d5a05a10950ef7315f504d4e632b5e2f909
SteveBinary <60712092+SteveBinary@users.noreply.github.com>
2026-02-27 13:28:31 +0100

    Adapt the CPU benchmark script to block screen lock and sleep.

--- pkgs/benchmark/cpu.nix
+++ pkgs/benchmark/cpu.nix
@@ -7,30 +7,44 @@ pkgs.writeShellApplication {
     geekbench
     mprime
     sysbench
+
+    coreutils
+    systemd
   ];
 
   text = ''
     WARMUP_TIME=''${1:-15m}
-    {
-      echo "================================================================================"
-      echo "= warmup for $(printf "%-32s" "$WARMUP_TIME")                                  ="
-      echo "================================================================================"
-      timeout --preserve-status "$WARMUP_TIME" mprime -t
-      # cleanup files created by mprime
-      rm {prime.txt,mprime.pid,results.bench.txt} 2&> /dev/null || true
+    systemd-inhibit --who "Benchmark CPU" --why "benchmark" bash <<EOF
+      {
+        echo "================================================================================"
+        echo "= warmup for $(printf "%-32s" "$WARMUP_TIME")                                  ="
+        echo "================================================================================"
+        timeout --preserve-status "$WARMUP_TIME" mprime -t
+        # cleanup files created by mprime
+        rm {prime.txt,mprime.pid,results.txt,results.bench.txt} 2&> /dev/null || true
 
-      echo
-      echo "================================================================================"
-      echo "= running Geekbench 6                                                          ="
-      echo "================================================================================"
-      echo
-      geekbench6 --cpu
+        echo
+        echo "================================================================================"
+        echo "= running Geekbench 6                                                          ="
+        echo "================================================================================"
+        echo
+        geekbench6 --cpu
 
-      echo "================================================================================"
-      echo "= running sysbench on all CPUs for 5 minutes                                   ="
-      echo "================================================================================"
-      echo
-      sysbench --threads="$(nproc)" --time=300 cpu run
-    } |& tee "benchmark-cpu__$(date +'%Y-%m-%d_%H-%M-%S').log"
+        echo "================================================================================"
+        echo "= running sysbench on all CPUs 3x for 2 minutes each                           ="
+        echo "================================================================================"
+        echo
+        echo "= run 1 of sysbench ============================================================"
+        echo
+        sysbench --threads="$(nproc)" --time=120 cpu run
+        echo "= run 2 of sysbench ============================================================"
+        echo
+        sysbench --threads="$(nproc)" --time=120 cpu run
+        echo "= run 3 of sysbench ============================================================"
+        echo
+        sysbench --threads="$(nproc)" --time=120 cpu run
+
+      } |& tee "benchmark-cpu__$(date +'%Y-%m-%d_%H-%M-%S').log"
+    EOF
   '';
 }
